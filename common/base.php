42359fbd36ff1d01637e782dd686356da485840d
baqarah <tomek.kimak@gmail.com>
2016-08-11 16:57:20 +0200

    base.php added

---
+++ common/base.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+include_once "inc/constants.inc.php";
+
+$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if (!$db) {
+    die("Failed to connect to MySQL: " .  mysqli_connect_error());
+}
+}
+?>
--- test_users.php
+++ test_users.php
@@ -1,6 +1,8 @@
 <?php
 include_once "inc/class.users.inc.php";
 include_once "inc/constants.inc.php";
+include_once "common/base.php";
+
 
 $users = new BSBUsers($db);
 
