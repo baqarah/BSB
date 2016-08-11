8e043da22aefa51a4cab0abc1d7bec618243a25d
baqarah <tomek.kimak@gmail.com>
2016-08-11 16:24:37 +0200

    pdo; zaczynam login

---
+++ common/pdo.php
@@ -0,0 +1,15 @@
+<?php
+erro_reporting(E_ALL);
+init_set("display_errors", 1);
+
+session_start();
+include_once "inc/constants.inc.php";
+
+try {
+    $dsn = "mysql:host=".DB_HOST."dbname=".DB_NAME;
+    $db = new PDO($dsn, DB_USER, DB_PASS);
+} catch (PDOException $e) {
+    echo 'Connection failed: '. $e->getMessage();
+    exit;
+}
+?>
