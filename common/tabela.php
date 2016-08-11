162399122eaee785dd0fe04f223f8280c52f96bb
baqarah <tomek.kimak@gmail.com>
2016-08-11 13:51:13 +0200

    czyszczenie plikow
    \

--- common/tabela.php
+++ common/tabela.php
@@ -1,44 +1,35 @@
-<?php
-
-echo "<tr>"; 
-echo "<td>" . $_COOKIE["cell0"] . "</td>";
-echo "<td>" . $_COOKIE["cell1"] . "</td>";
-echo "<td>" . $_COOKIE["cell2"] . "</td>";
-echo "<td>" . $_COOKIE["cell3"] . "</td>";
-echo "<td>" . $_COOKIE["cell4"] . "</td>";
-echo "</tr>";
-
-echo "<tr>"; 
-echo "<td>" . $_COOKIE["cell5"] . "</td>";
-echo "<td>" . $_COOKIE["cell6"] . "</td>";
-echo "<td>" . $_COOKIE["cell7"] . "</td>";
-echo "<td>" . $_COOKIE["cell8"] . "</td>";
-echo "<td>" . $_COOKIE["cell9"] . "</td>";
-echo "</tr>";
-
-echo "<tr>"; 
-echo "<td>" . $_COOKIE["cell10"] . "</td>";
-echo "<td>" . $_COOKIE["cell11"] . "</td>";
-echo "<td bgcolor='red'>" . $_COOKIE["cell12"] . "</td>";
-echo "<td>" . $_COOKIE["cell13"] . "</td>";
-echo "<td>" . $_COOKIE["cell14"] . "</td>";
-echo "</tr>";
-
-echo "<tr>"; 
-echo "<td>" . $_COOKIE["cell15"] . "</td>";
-echo "<td>" . $_COOKIE["cell16"] . "</td>";
-echo "<td>" . $_COOKIE["cell17"] . "</td>";
-echo "<td>" . $_COOKIE["cell18"] . "</td>";
-echo "<td>" . $_COOKIE["cell19"] . "</td>";
-echo "</tr>";
-
-echo "<tr>"; 
-echo "<td>" . $_COOKIE["cell20"] . "</td>";
-echo "<td>" . $_COOKIE["cell21"] . "</td>";
-echo "<td>" . $_COOKIE["cell22"] . "</td>";
-echo "<td>" . $_COOKIE["cell23"] . "</td>";
-echo "<td>" . $_COOKIE["cell24"] . "</td>";
-echo "</tr>";
-
-?>
-
+<tr>
+    <td><?php echo $_COOKIE["cell0"] ?></td>
+    <td><?php echo $_COOKIE["cell1"] ?></td>
+    <td><?php echo $_COOKIE["cell2"] ?></td>
+    <td><?php echo $_COOKIE["cell3"] ?></td>
+    <td><?php echo $_COOKIE["cell4"] ?></td>
+</tr>
+<tr>
+    <td><?php echo $_COOKIE["cell5"] ?></td>
+    <td><?php echo $_COOKIE["cell6"] ?></td>
+    <td><?php echo $_COOKIE["cell7"] ?></td>
+    <td><?php echo $_COOKIE["cell8"] ?></td>
+    <td><?php echo $_COOKIE["cell9"] ?></td>
+</tr>
+<tr>
+    <td><?php echo $_COOKIE["cell10"] ?></td>
+    <td><?php echo $_COOKIE["cell11"] ?></td>
+    <td bgcolor="red"><?php echo $_COOKIE["cell12"] ?></td>
+    <td><?php echo $_COOKIE["cell13"] ?></td>
+    <td><?php echo $_COOKIE["cell14"] ?></td>
+</tr>
+<tr>
+    <td><?php echo $_COOKIE["cell15"] ?></td>
+    <td><?php echo $_COOKIE["cell16"] ?></td>
+    <td><?php echo $_COOKIE["cell17"] ?></td>
+    <td><?php echo $_COOKIE["cell18"] ?></td>
+    <td><?php echo $_COOKIE["cell19"] ?></td>
+</tr>
+<tr>
+    <td><?php echo $_COOKIE["cell20"] ?></td>
+    <td><?php echo $_COOKIE["cell21"] ?></td>
+    <td><?php echo $_COOKIE["cell22"] ?></td>
+    <td><?php echo $_COOKIE["cell23"] ?></td>
+    <td><?php echo $_COOKIE["cell24"] ?></td>
+</tr>    
