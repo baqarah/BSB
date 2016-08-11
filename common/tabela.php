ef41ac4c3bf5dfc439eba50aa8901f3b7c023b0a
baqarah <tomek.kimak@gmail.com>
2016-08-11 11:39:52 +0200

    dzielenie index.php, script.js, tabela.php

---
+++ common/tabela.php
@@ -0,0 +1,44 @@
+<?php
+
+echo "<tr>"; 
+echo "<td>" . $_COOKIE["cell0"] . "</td>";
+echo "<td>" . $_COOKIE["cell1"] . "</td>";
+echo "<td>" . $_COOKIE["cell2"] . "</td>";
+echo "<td>" . $_COOKIE["cell3"] . "</td>";
+echo "<td>" . $_COOKIE["cell4"] . "</td>";
+echo "</tr>";
+
+echo "<tr>"; 
+echo "<td>" . $_COOKIE["cell5"] . "</td>";
+echo "<td>" . $_COOKIE["cell6"] . "</td>";
+echo "<td>" . $_COOKIE["cell7"] . "</td>";
+echo "<td>" . $_COOKIE["cell8"] . "</td>";
+echo "<td>" . $_COOKIE["cell9"] . "</td>";
+echo "</tr>";
+
+echo "<tr>"; 
+echo "<td>" . $_COOKIE["cell10"] . "</td>";
+echo "<td>" . $_COOKIE["cell11"] . "</td>";
+echo "<td bgcolor='red'>" . $_COOKIE["cell12"] . "</td>";
+echo "<td>" . $_COOKIE["cell13"] . "</td>";
+echo "<td>" . $_COOKIE["cell14"] . "</td>";
+echo "</tr>";
+
+echo "<tr>"; 
+echo "<td>" . $_COOKIE["cell15"] . "</td>";
+echo "<td>" . $_COOKIE["cell16"] . "</td>";
+echo "<td>" . $_COOKIE["cell17"] . "</td>";
+echo "<td>" . $_COOKIE["cell18"] . "</td>";
+echo "<td>" . $_COOKIE["cell19"] . "</td>";
+echo "</tr>";
+
+echo "<tr>"; 
+echo "<td>" . $_COOKIE["cell20"] . "</td>";
+echo "<td>" . $_COOKIE["cell21"] . "</td>";
+echo "<td>" . $_COOKIE["cell22"] . "</td>";
+echo "<td>" . $_COOKIE["cell23"] . "</td>";
+echo "<td>" . $_COOKIE["cell24"] . "</td>";
+echo "</tr>";
+
+?>
+
