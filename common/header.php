f4e23bdf4370b64318354b7685585523950f0faa
baqarah <tomek.kimak@gmail.com>
2016-08-12 13:28:47 +0200

    signup pierwszy widok

--- common/header.php
+++ common/header.php
@@ -1,5 +1,5 @@
 <head>
-    <title>BSBingo</title>
+    <title>BSBingo | <?php echo $pageTitle ?></title>
     <link rel="stylesheet" type="text/css" href="style.css" />
     <link rel="shortcut icon"  type"image/x-icon href="icon.ico" />
 </head>
