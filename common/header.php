e1cee255396a457b85a36bc6a357daa643737eac
baqarah <tomek.kimak@gmail.com>
2016-08-16 14:27:12 +0200

    testowanie stylow

--- common/header.php
+++ common/header.php
@@ -4,10 +4,9 @@
     <link rel="shortcut icon"  type"image/x-icon href="icon.ico" />
 
 
-    <header class="main-header">
-        
-    
+    <header class="main-header">    
         <img src="common/logo.png" alt="BSB" style="width:100px;height:100px;" >
+        <header class="content">Testowanie scierwa</header>
     </header>
 
 </head>
