00d79cbcb4f1a3975f2ffbbe766215ba2b7b037b
baqarah <tomek.kimak@gmail.com>
2016-08-16 12:53:25 +0200

    test logo

--- common/header.php
+++ common/header.php
@@ -2,6 +2,8 @@
     <title>BSBingo | <?php echo $pageTitle ?></title>
     <link rel="stylesheet" type="text/css" href="style.css" />
     <link rel="shortcut icon"  type"image/x-icon href="icon.ico" />
+    <h3>test</h3>
+    <img src="common/logo.png" alt="BSB" style="width:100px;height:100px;" >
     <h1>Witam w BSB Project!</h1>
     <h3>enjoy yor stay</h3><br><br>
 
