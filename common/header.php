03d54647e84f0a25a1e5e9de01bff2617739b8fd
baqarah <tomek.kimak@gmail.com>
2016-08-16 14:41:29 +0200

    viewport

--- common/header.php
+++ common/header.php
@@ -1,4 +1,6 @@
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+
     <title>BSBingo | <?php echo $pageTitle ?></title>
     <link rel="stylesheet" type="text/css" href="style.css" />
     <link rel="shortcut icon"  type"image/x-icon href="icon.ico" />
