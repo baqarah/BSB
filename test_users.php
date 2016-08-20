04da7bcd227869e24205b2c730dc0af02a4cb5a9
baqarah <tomek.kimak@gmail.com>
2016-08-20 21:34:57 +0200

    dodatanie login.php

--- test_users.php
+++ test_users.php
@@ -1,9 +1,26 @@
 <?php
-include_once "inc/class.users.inc.php";
-include_once "inc/constants.inc.php";
 include_once "common/base.php";
+$pageTitle = "Home";
+include_once "common/header.php";
 
-
-$users = new BSBUsers($db);
-echo $users->createAccount();
 ?>
+
+<h2>Login failed, try again:</h2>
+<form method="post" action="login.php" id="loginform">
+    <div>
+        <label for="username">User Name:</label>
+        <input type="text" name="username" id="username" /><br>
+        
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" /><br>
+        
+        <input type="submit" name="login" id="login" value="Login" />
+    </div>
+</form>
+
+<?php
+
+include_once "common/footer.php";
+
+?>
+
