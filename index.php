43b298d115d4bae235475395ce821ebe4dc4b199
baqarah <tomek.kimak@gmail.com>
2016-08-24 17:48:38 +0200

    naprawianie  wszystkiego

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 $path = $_SERVER['DOCUMENT_ROOT'];
 $pageTitle = "Home";
 
-include_once "common/base.php";
-include_once "common/cookie.php";
-include_once "common/header.php";
+include_once $path . "/common/base.php";
+include_once $path . "/common/cookie.php";
+include_once $path . "/common/header.php";
 
 if ($_SESSION['LoggedIn'] == 1) { 
-    include_once 'common/menu.php';
+    include_once $path . "/common/menu.php";
 } else {
     echo '<h2 class="tytul">Login first</h2>';
 }
@@ -18,4 +18,4 @@ if ($_SESSION['LoggedIn'] == 1) {
 Zeby zresetowac cookiesy: <a href="http://89.77.118.160:8080/cook.php">Kliknij tutaj</a> 
 <script src = "common/script.js"></script>
 
-<?php include_once "common/footer.php"; ?>
+<?php include_once $path . "/common/footer.php"; ?>
