32aa79c6e617cedf5580c7ade619c3f272e7d19b
baqarah <tomek.kimak@gmail.com>
2016-08-24 17:47:07 +0200

    ' ' w DB_HOST

--- common/base.php
+++ common/base.php
@@ -3,7 +3,7 @@ session_start();
 
 include_once $path . "/inc/constants.inc.php"; 
 
-$db = mysqli_connect('DB_HOST', DB_USER, DB_PASS, DB_NAME);
+$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if (!$db) {
     die("Failed to connect to MySQL: " .  mysqli_connect_error());
 }
