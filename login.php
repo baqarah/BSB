222245031f67caa680a55f0e8bb0019fb94b46f2
baqarah <tomek.kimak@gmail.com>
2016-08-24 09:55:45 +0200

    debug

--- login.php
+++ login.php
@@ -4,24 +4,18 @@ $pageTitle = "Home";
 include_once "common/header.php";
 
 
-echo !empty($_SESSION['LoggedIn']);
-echo !empty($_SESSION['Username']);
 
-if 
+if (!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username'])) {
+    echo "<p>You are currently logged in.</p>";
+    exit;
+}
 
-//if (!empty($_SESSION['LoggedIn']) $$ !empty($_SESSION['Username'])) {
-//    echo "<p>You are currently logged in.</p>";
-//    exit;
-//}
-
-
-    /*
-} elseif (!empty($_POST['username']) && !empty($_POST['password'])) {
+/*
+if (!empty($_POST['username']) && !empty($_POST['password'])) {
     include_once "inc/class.users.inc.php";
     $users = new BSBUsers($db);
     if ($users->accountLogin() == TRUE) {
-        echo "test";
-        //echo "<meta http-equiv='refresh' content='0;/'>";
+        echo "<meta http-equiv='refresh' content='0;/'>";
         exit;
     } else {       
 
@@ -29,12 +23,10 @@ if
 
     };        
 } elseif (empty($_POST['username']) || empty($_POST['password'])) {
-
-        echo "tutaj bedzie formularz normalny";
-    
+        echo "tutaj bedzie formularz normalny";  
 }
 
--->*/
+*/
                                
 include_once "common/footer.php";
 
