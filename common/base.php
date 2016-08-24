c915e6fe940569f8653a818486e811837785efeb
baqarah <tomek.kimak@gmail.com>
2016-08-24 17:01:13 +0200

    linki sie kaszania

--- common/base.php
+++ common/base.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include_once "http://89.77.118.160:8080/inc/constants.inc.php"; 
+include_once "inc/constants.inc.php"; 
 
 $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if (!$db) {
