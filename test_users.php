a59f2e6b04727fd888f8865716b3d057b0c66467
baqarah <tomek.kimak@gmail.com>
2016-08-24 17:25:03 +0200

    zwalilem wszysktkie linikk

--- test_users.php
+++ test_users.php
@@ -1,26 +1,9 @@
 <?php
-include_once "common/base.php";
-$pageTitle = "Home";
-include_once "common/header.php";
 
-?>
-
-<h2>Login failed, try again:</h2>
-<form method="post" action="login.php" id="loginform">
-    <div>
-        <label for="username">User Name:</label>
-        <input type="text" name="username" id="username" /><br>
-        
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" /><br>
-        
-        <input type="submit" name="login" id="login" value="Login" />
-    </div>
-</form>
-
-<?php
-
-include_once "common/footer.php";
+$path=$_SERVER['DOCUMEN_ROOT'];
+echo $path;
+echo "<br>"
+echo $path . "/costam/"
 
 ?>
 
