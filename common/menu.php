ee37c38bfe5e0959275619fb9eaeb76d1da78f5b
baqarah <tomek.kimak@gmail.com>
2016-08-25 11:06:47 +0200

    dodawanie obrazkow do buttonow

--- common/menu.php
+++ common/menu.php
@@ -2,12 +2,30 @@
     <div id="menu-header" >
         <h2 class="tytul"> Whatcha need?</h2>
         <div id="menu">
-            <a href="/common/bingo.php" class="menu-button" style="background-color: #73bec8">Bingo jest tutaj</a>
-            <a href="/"  class="menu-button" style="background-color: #e4ba3f" >Test</a>
-            <a href="/"  class="menu-button" style="background-color: #cb732b">Test</a>
-            <a href="/"  class="menu-button" style="background-color: #89bc62">Test</a>
-            <a href="/"  class="menu-button" style="background-color: #c384c5">Test</a>
-            <a href="/"  class="menu-button" style="background-color: #c85977">Test</a>
+            <a href="/common/bingo.php" class="menu-button" style="background-color: #73bec8">
+                <img src="/common/menu/bingo.png"  class="menu-pic">
+                <span class="menu-text">Bingo jest tutaj</span>
+            </a>
+            <a href="/"  class="menu-button" style="background-color: #e4ba3f" >
+                <img src="/common/menu/bingo.png" class="menu-pic" >
+                <span class="menu-text" >User</span>
+            </a>
+            <a href="/"  class="menu-button" style="background-color: #cb732b">
+
+                <span class="menu-text" >Tutaj tekst</span>
+            </a>
+            <a href="/"  class="menu-button" style="background-color: #89bc62">
+
+                <span class="menu-text" >Tutaj tekst</span>
+            </a>
+            <a href="/"  class="menu-button" style="background-color: #c384c5">
+
+                <span class="menu-text" >Tutaj tekst</span>
+            </a>
+            <a href="/"  class="menu-button" style="background-color: #c85977">
+
+                <span class="menu-text" >Tutaj tekst</span>
+            </a>
         </div>
     </div>
 </div>
