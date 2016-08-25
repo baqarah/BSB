1dcdaedf5a0d3f5f4cbec879db9e95a8e2e5d3c5
baqarah <tomek.kimak@gmail.com>
2016-08-25 10:14:52 +0200

    czcionki w menu

--- common/menu.php
+++ common/menu.php
@@ -2,12 +2,12 @@
     <div id="menu-header" >
         <h2 class="tytul"> Whatcha need?</h2>
         <div id="menu">
-            <a href="/common/bingo.php" class="menu-button" >Bingo jest tutaj</a>
-            <a href="/"  class="menu-button" >Test</a>
-            <a href="/"  class="menu-button" >Test</a>
-            <a href="/"  class="menu-button" >Test</a>
-            <a href="/"  class="menu-button" >Test</a>
-            <a href="/"  class="menu-button" >Test</a>
+            <a href="/common/bingo.php" class="menu-button" style="background-color: #73bec8">Bingo jest tutaj</a>
+            <a href="/"  class="menu-button" style="background-color: #e4ba3f" >Test</a>
+            <a href="/"  class="menu-button" style="background-color: #cb732b">Test</a>
+            <a href="/"  class="menu-button" style="background-color: #89bc62">Test</a>
+            <a href="/"  class="menu-button" style="background-color: #c384c5">Test</a>
+            <a href="/"  class="menu-button" style="background-color: #c85977">Test</a>
         </div>
     </div>
 </div>
