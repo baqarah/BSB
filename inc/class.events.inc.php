c83b3c8aaf6217e2e66d6d0b91d3ff4ffb00cf0f
baqarah <tomek.kimak@gmail.com>
2016-08-29 13:25:33 +0200

    linia po linii

--- inc/class.events.inc.php
+++ inc/class.events.inc.php
@@ -28,10 +28,10 @@ class Events
               ."FROM Events e, Events_Rozdanie b, Rozdanie r "
               ."WHERE e.ID_Event = b.ID_Event "
               ."AND b.ID_Rozdanie = r.ID_Rozdanie "
-              ."AND e.Aktywny = 1 AND r.UserID =" . $this->_userid;
+              ."AND e.Aktywny = 1 AND r.UserID =1"// . $this->_userid;
         
         $result = $this->_db->query($sql);
-        $row = $result->fetch_assoc();
+        //$row = $result->fetch_assoc();
         //$this->_activelist = $row['IDs'];
     }
 
@@ -59,7 +59,7 @@ class Events
 
     public function testas()
     {
-        return $this->_activelist;
+        return $this->_userid;
     }   
     
 }
