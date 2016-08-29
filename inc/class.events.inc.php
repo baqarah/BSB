a7b290f7755c7ef0bf742b5cf7d3cc3eb3a2d8ae
baqarah <tomek.kimak@gmail.com>
2016-08-29 13:19:06 +0200

    debug linia po linii

--- inc/class.events.inc.php
+++ inc/class.events.inc.php
@@ -30,9 +30,9 @@ class Events
               ."AND b.ID_Rozdanie = r.ID_Rozdanie "
               ."AND e.Aktywny = 1 AND r.UserID =1"; //. $this->_userid;
         
-        $result = $this->_db->query($sql);
-        $row = $result->fetch_assoc();
-        $this->_activelist = $row['IDs'];
+        //$result = $this->_db->query($sql);
+        //$row = $result->fetch_assoc();
+        //$this->_activelist = $row['IDs'];
     }
 
     public function createEvent()
