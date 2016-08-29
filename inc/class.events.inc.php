631b2b21fc7a7500149d0c9ab63c1faa4990c26f
baqarah <tomek.kimak@gmail.com>
2016-08-29 13:33:44 +0200

    kurwa nie wiem

--- inc/class.events.inc.php
+++ inc/class.events.inc.php
@@ -30,8 +30,8 @@ class Events
               ."AND b.ID_Rozdanie = r.ID_Rozdanie "
               ."AND e.Aktywny = 1 AND r.UserID =" . $this->_userid;
         
-        $result = $this->_db->query($sql);
-        $row = $result->fetch_assoc(); 
+        $rezult = $this->_db->query($sql);
+        $row = $rezult->fetch_assoc(); 
         //$this->_activelist = $row['IDs'];
     }
 
