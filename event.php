c6a9d09a127392f664edb6101c3eb68522de724b
baqarah <tomek.kimak@gmail.com>
2016-08-29 12:05:47 +0200

    testowanie sqla

--- event.php
+++ event.php
@@ -5,7 +5,7 @@ include_once $pth . "/common/header.php";
 include_once $pth . "/inc/class.events.inc.php";
 
 $events = new Events;
-echo "ID User:" . $events->testUserId() . "koniec";
+echo "ID User:" . "asdasd" . "koniec";
 
 echo "<h2>Tutaj bedzie strona z eventami wszystkimi</h2>";
 echo '<a href="/event_add.php">DODAJ EVENT</a>';
