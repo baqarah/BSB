b9a9a45fd9339f2772c6d09b022edaae7d9fcda3
baqarah <tomek.kimak@gmail.com>
2016-08-31 17:07:57 +0200

    blad w eotcie?

--- inc/class.events.inc.php
+++ inc/class.events.inc.php
@@ -111,7 +111,6 @@ class Events
         FROM Events e, Events_Rozdanie b, Rozdanie r, Users u 
         WHERE e.ID_Event = $id
         AND e.ID_Event = b.ID_Event AND b.ID_Rozdanie = r.ID_Rozdanie AND r.UserID = u.UserID EOT;
-        
         $result = $this->_db->query($sql);
         
         while ($row = $result->fetch_assoc()) {
