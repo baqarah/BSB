f31ff56f2513d42a122bb168012b5f96c648787a
baqarah <tomek.kimak@gmail.com>
2016-08-31 17:14:55 +0200

    EOT?

--- inc/class.events.inc.php
+++ inc/class.events.inc.php
@@ -101,16 +101,17 @@ class Events
 
     public function showEvent($id)
     {
-//        $nazwa = "";
-//        $start = "";
-//        $koniec = "";
+        $nazwa = "";
+        $start = "";
+        $koniec = "";
         $arrayuser = array();
         
         $sql = <<<EOT
         SELECT e.EventNazwa as Nazwa, e.EventStart as Start, e.EventEnd as Koniec, u.UserName as User 
         FROM Events e, Events_Rozdanie b, Rozdanie r, Users u 
         WHERE e.ID_Event = $id
-        AND e.ID_Event = b.ID_Event AND b.ID_Rozdanie = r.ID_Rozdanie AND r.UserID = u.UserID EOT;
+        AND e.ID_Event = b.ID_Event AND b.ID_Rozdanie = r.ID_Rozdanie AND r.UserID = u.UserID
+EOT;
 
         echo $sql;
         
@@ -118,9 +119,9 @@ class Events
         
         while ($row = $result->fetch_assoc()) {
             //$row = $result->fetch_assoc();
-            //$nazwa = $row['Nazwa'];
-            //$start = $row['Start'];
-            //$koniec = $row['Koniec'];
+            $nazwa = $row['Nazwa'];
+            $start = $row['Start'];
+            $koniec = $row['Koniec'];
             $arrayuser[] = $row["User"];
         }
         
