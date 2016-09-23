f11f64f6d2c180a6080d2e818ae0569bfebafc7d
baqarah <tomek.kimak@gmail.com>
2016-09-23 16:16:48 +0200

    testwoaniea

--- test_rozdan.php
+++ test_rozdan.php
@@ -1,21 +1,22 @@
-<?php
-echo "<h> Test rozdan<h>";
-$pth = $_SERVER['DOCUMENT_ROOT'];
+/*<?php
+  echo "<h> Test rozdan<h>";
+  $pth = $_SERVER['DOCUMENT_ROOT'];
+  
+  
+  include_once $pth . "/common/base.php";
+  include_once $pth . "/common/header.php";
+  include_once $pth . "/inc/class.rozdanie.inc.php";
+  
+  $rozdanie = new Rozdanie($db, 42);
+  //print_r($rozdanie->getHits());
+  //print_r($rozdanie->getTxts());
+  //print_r($rozdanie->getIDTxts());
+  //echo "<br>";
+  $rozdanie->showRozdanie();
 
+  
+  ?> */
 
-include_once $pth . "/common/base.php";
-include_once $pth . "/common/header.php";
-include_once $pth . "/inc/class.rozdanie.inc.php";
-
-$rozdanie = new Rozdanie($db, 42);
-//print_r($rozdanie->getHits());
-//print_r($rozdanie->getTxts());
-//print_r($rozdanie->getIDTxts());
-//echo "<br>";
-$rozdanie->showRozdanie();
-
-
-?>
 <button type="button" onlick="funsql();" >klikmnie</button>
 <p><span id="text" >UMCUMSCUMSC</span></p>
 <script>
@@ -36,7 +37,7 @@ $rozdanie->showRozdanie();
          }
      };
 
-     xhttp.open("GET", "test_ajax.php", true);
+     xhttp.open("GET", "test_ajax.txt", true);
      xhttp.send();
  
  }
