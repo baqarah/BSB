092187b2b615365eaebf13e412c1cdf64a6028c7
baqarah <tomek.kimak@gmail.com>
2016-09-23 16:29:58 +0200

    tesraz juz kurwa nie wiem

---
+++ test_ajax2.php
@@ -0,0 +1,11 @@
+
+<button type="button" onlick="funsql();" >klikmnie</button>
+<p><span id="text" >UMCUMSCUMSC</span></p>
+<script>
+ function funsql() {
+     var xhttp;
+     alert("kurwa");
+}
+
+</script>
+
