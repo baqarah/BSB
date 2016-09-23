f5bbc25fdd824a8f3038538ab3f2aa76ece71bf2
baqarah <tomek.kimak@gmail.com>
2016-09-23 11:16:35 +0200

    testowanie bazy

--- inc/class.rozdanie.inc.php
+++ inc/class.rozdanie.inc.php
@@ -3,9 +3,10 @@ class Rozdanie
 {
 
     private $_db;
-    private $_rozdanie_info;
-    //private $_bstexts;
-    //private $_hits;
+    //private $_rozdanie_info;
+    private $_txts;
+    private $_idtxts;
+    private $_hits;
     
     
     public function __construct($db = NULL, $id_rozdanie)
@@ -19,35 +20,29 @@ class Rozdanie
         //$sql = "SELECT r.ID_BS id, r.CzyTrafione hit, s.BSText txt FROM LinieRozdan as r JOIN Bshits as s WHERE ID_Rozdanie = " . $id_rozdanie;  
         $sql = "SELECT r.PoleRozdania pole, r.ID_BS id, r.CzyTrafione hit, s.BSText txt " 
               ."FROM BSB.LinieRozdan as r JOIN BSB.BShits as s ON r.ID_BS = s.ID_BS WHERE r.ID_Rozdanie = " . $id_rozdanie;
-        $this->_rozdanie_info = $this->_db->query($sql);
-                   
+        $rozdanie_info = $this->_db->query($sql);
+        
+        while ($row = $this->$rozdanie_info->fetch_assoc()) {
+            $this->_txts[] = $row['txt'];
+            $this->_idtxts[] = $row['id'];
+            $this->_hits[] = $row['hit'];
+        }
+            
     }
 
     public function getHits()
     {
-        $a = array();
-        while ($row = $this->_rozdanie_info->fetch_assoc()) {
-            $a[]=$row['hit'];
-        }
-        return $a;
+        return $this->_hits;
     }
 
     public function getTxts()
     {
-        $a = array();
-        while ($row = $this->_rozdanie_info->fetch_assoc()) {
-            $a[]=$row['txt'];
-        }
-        return $a;
+        return $this->_txts;
     }
 
     public function getIDTxts()
     {
-        $a = array();
-        while ($row = $this->_rozdanie_info->fetch_assoc()) {
-            $a[]=$row['id'];
-        }
-        return $a;
+        return $this->_idtxts;
     }
 
     public function showRozdanie()
