d7e7ce00df578c627a39c0886883f259284e8d67
baqarah <tomek.kimak@gmail.com>
2016-09-23 14:56:55 +0200

    testowania ajaxa cd

--- test_rozdan.php
+++ test_rozdan.php
@@ -17,17 +17,16 @@ $rozdanie->showRozdanie();
 
 ?>
 <button type="button" onlick="eskuel()" >klikmnie</button>
-<p><span id="text" ></span></p>
+<p><span id="text" >UMCUMSCUMSC</span></p>
 <script>
  function eskuel() {
      var xhttp;
      if (window.XMLHttpRequest) {
-         // code for modern browsers
          xhttp = new XMLHttpRequest();
      } else {
-         // code for IE6, IE5
          xhttp = new ActiveXObject("Microsoft.XMLHTTP");
      }
+     document.getElementById("text").innerHTML = "eloszka"
      document.getElementById("text").innerHTML = this.responseText;
      xhttp.open("GET", "test_ajax.php", true);
      xhttp.send();
