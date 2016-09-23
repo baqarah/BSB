14aadffae173abcbeb1467781ea6f0b81e50f9e9
baqarah <tomek.kimak@gmail.com>
2016-09-23 12:40:35 +0200

    przypisanie getTxts do zmienne

--- inc/class.showrozdanie.php
+++ inc/class.showrozdanie.php
@@ -1,38 +1,40 @@
+<?php $a = $this->getTxts() ?>
+
 <table id="MyTable">
 <tr>
-    <td><?php echo $this->getTxts[0] ?></td>
-    <td><?php echo $this->getTxts[1] ?></td>
-    <td><?php echo $this->getTxts[2] ?></td>
-    <td><?php echo $this->getTxts[3] ?></td>
-    <td><?php echo $this->getTxts[4] ?></td>
+    <td><?php echo $a[0] ?></td>
+    <td><?php echo $a[1] ?></td>
+    <td><?php echo $a[2] ?></td>
+    <td><?php echo $a[3] ?></td>
+    <td><?php echo $a[4] ?></td>
 </tr>
 <tr>
-    <td><?php echo $this->getTxts[5] ?></td>
-    <td><?php echo $this->getTxts[6] ?></td>
-    <td><?php echo $this->getTxts[7] ?></td>
-    <td><?php echo $this->getTxts[8] ?></td>
-    <td><?php echo $this->getTxts[9] ?></td>
+    <td><?php echo $a[5] ?></td>
+    <td><?php echo $a[6] ?></td>
+    <td><?php echo $a[7] ?></td>
+    <td><?php echo $a[8] ?></td>
+    <td><?php echo $a[9] ?></td>
 </tr>
 <tr>
-    <td><?php echo $this->getTxts[10] ?></td>
-    <td><?php echo $this->getTxts[11] ?></td>
-    <td><?php echo $this->getTxts[12] ?></td>
-    <td><?php echo $this->getTxts[13] ?></td>
-    <td><?php echo $this->getTxts[14] ?></td>
+    <td><?php echo $a[10] ?></td>
+    <td><?php echo $a[11] ?></td>
+    <td><?php echo $a[12] ?></td>
+    <td><?php echo $a[13] ?></td>
+    <td><?php echo $a[14] ?></td>
 </tr>
 <tr>
-    <td><?php echo $this->getTxts[15] ?></td>
-    <td><?php echo $this->getTxts[16] ?></td>
-    <td><?php echo $this->getTxts[17] ?></td>
-    <td><?php echo $this->getTxts[18] ?></td>
-    <td><?php echo $this->getTxts[19] ?></td>
+    <td><?php echo $a[15] ?></td>
+    <td><?php echo $a[16] ?></td>
+    <td><?php echo $a[17] ?></td>
+    <td><?php echo $a[18] ?></td>
+    <td><?php echo $a[19] ?></td>
 </tr>
 <tr>
-    <td><?php echo $this->getTxts[20] ?></td>
-    <td><?php echo $this->getTxts[21] ?></td>
-    <td><?php echo $this->getTxts[22] ?></td>
-    <td><?php echo $this->getTxts[23] ?></td>
-    <td><?php echo $this->getTxts[24] ?></td>
+    <td><?php echo $a[20] ?></td>
+    <td><?php echo $a[21] ?></td>
+    <td><?php echo $a[22] ?></td>
+    <td><?php echo $a[23] ?></td>
+    <td><?php echo $a[24] ?></td>
 </tr>
 </table>
 
