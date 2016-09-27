85c2ad18faf95206d7f3c1074e03f15945837756
baqarah <tomek.kimak@gmail.com>
2016-09-27 17:42:40 +0200

    literowka

--- common/bingo.php
+++ common/bingo.php
@@ -5,7 +5,7 @@ $pageTitle = "Tabela";
 include_once $pth . "/common/base.php";
 //include_once $pth . "/common/cookie.php";
 include_once $pth . "/common/header.php";
-include_once $pth . "inc/class.events.inc.php";
+include_once $pth . "/inc/class.events.inc.php";
 include_once $pth . "/inc/class.rozdanie.inc.php";
 
 
