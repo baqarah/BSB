a8f8e6bcb2499bdcbe9f698985218a14f4f1b72e
baqarah <tomek.kimak@gmail.com>
2016-09-27 15:32:53 +0200

    usuniecie jednej z tabel

--- common/bingo.php
+++ common/bingo.php
@@ -9,19 +9,12 @@ include_once $pth . "/inc/class.rozdanie.inc.php";
 
 
 if ($_SESSION['LoggedIn'] == 1) { 
-    include_once $pth . "/common/tabela.php";
-
+    //include_once $pth . "/common/tabela.php";
     $rozdanie = new Rozdanie($db, 43);
     $rozdanie->showRozdanie();
-    
-
-
 } 
 ?>
 
-<br>
-<br>
-Zeby zresetowac cookiesy: <a href="http://89.77.118.160:8080/cook.php">Kliknij tutaj</a> 
 <script src = "/common/script.js"></script>
 
 <?php include_once $pth . "/common/footer.php"; ?>
