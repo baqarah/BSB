9d13ddf2440be54699f78e4fdfd2d5d53d09c108
baqarah <tomek.kimak@gmail.com>
2016-09-27 15:46:38 +0200

    wynik calkiem z bazy

--- common/bingo.php
+++ common/bingo.php
@@ -12,15 +12,17 @@ if ($_SESSION['LoggedIn'] == 1) {
     //include_once $pth . "/common/tabela.php";
     $rozdanie = new Rozdanie($db, 43);
     $rozdanie->showRozdanie();
+
+    $h=$rozdanie->getHits();
 } 
 ?>
 <script>
  var wynik =[
-     [0,0,0,0,0],
-     [0,0,0,0,0],
-     [0,0,1,0,0],
-     [0,0,0,0,0],
-     [0,0,0,0,0]
+     [<?php echo $h[0] . "," . $h[1]. "," .$h[2] . "," $h[3]. "," $h[4] ?>],
+     [<?php echo $h[5] . "," . $h[6]. "," .$h[7] . "," $h[8]. "," $h[9] ?>],
+     [<?php echo $h[10] . "," . $h[11]. "," .$h[12] . "," $h[13]. "," $h[14] ?>],
+     [<?php echo $h[15] . "," . $h[16]. "," .$h[17] . "," $h[18]. "," $h[19] ?>],
+     [<?php echo $h[20] . "," . $h[21]. "," .$h[22] . "," $h[23]. "," $h[24] ?>]
  ]; 
 </script>
 <script type="text/javascript" src = "/common/script.js"></script>
