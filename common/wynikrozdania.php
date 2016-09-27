a84421c737cb011c672eff7a0866142b2fd32a4f
baqarah <tomek.kimak@gmail.com>
2016-09-27 15:04:50 +0200

    +1 w pole

--- common/wynikrozdania.php
+++ common/wynikrozdania.php
@@ -4,7 +4,7 @@ include_once $pth . "/common/base.php";
 include_once $pth . "/inc/class.rozdanie.inc.php";
 
 $rozdaniedozmiany = new Rozdanie($db, $_GET['idrozdania']);
-$rozdaniedozmiany->changeHit($_GET['pole'], $_GET['wynik']);
+$rozdaniedozmiany->changeHit($_GET['pole'] + 1, $_GET['wynik']);
 echo "ID: " . $_GET['idrozdania'] . ", Pole: " . $_GET['pole'] . ", Wynik: " . $_GET['wynik']; 
 
 
